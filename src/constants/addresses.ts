5dcf935db3a4446b49bfd456b6857f631f896f33
Mike Solomon <mike.solomon@coinbase.com>
2024-10-09 20:09:58 -0400

    update address to increased max bet contract

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,2 +1,2 @@
-export const BLACKJACK: `0x${string}` = "0xdfe2c810ae7bb2b0d897d96b578312e0b737d734";
+export const BLACKJACK: `0x${string}` = "0xbc43928e2b13e9d5af85a2e49d89098312bd6d52";
 export const CARD_NFT: `0x${string}` = "0x99Df777F316997f976bD36E6D02EA5794b12f5b7";
